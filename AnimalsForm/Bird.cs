e32761c6ceecb7f6c5da2b5431c39232fece0477
Theo Holmgren <theokodar@gmail.com>
2020-12-02 09:15:51 +0100

    Add project files.

---
+++ AnimalsForm/Bird.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsForm
+{
+    class Bird : Animal
+    {
+        public Bird(string name, int age) : base()
+        {
+            /*initiera attributen i objektet*/
+            SetSpecies(SPECIES.BIRD); // CAT=0, DOG=1, BIRD=2
+            SetCanFly(true);
+            SetName(name);
+            SetAge(age);
+        }
+
+        public override void IntroduceYourself()
+        {
+            Console.WriteLine("Piip. Jag är en fågel som heter " + GetName());
+            base.IntroduceYourself();
+        }
+
+    }
+}
---
+++ AnimalsForm/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsForm
+{
+    class Cat : Animal
+    {
+
+        public Cat(string name, int age) : base()
+        {
+            /*initiera attributen i objektet*/
+            SetSpecies(SPECIES.CAT); // CAT=0, DOG=1, BIRD=2
+            SetCanFly(false);
+            SetName(name);
+            SetAge(age);
+
+        }
+        public override void IntroduceYourself()
+        {
+            Console.WriteLine("Mjau. Jag är en katt som heter " + GetName());
+            base.IntroduceYourself();
+        }
+
+
+    }
+}
