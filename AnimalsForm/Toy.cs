0112abfdc52312c607386254112e3a4493b7cfff
Theo Holmgren <theokodar@gmail.com>
2020-12-10 11:48:38 +0100

    Worked in groups

---
+++ AnimalsForm/Toy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsForm
+{
+    class Toy
+    {
+        private string name;
+        public Toy(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            string name = this.name;
+            return name;
+        }
+    }
+}
---
+++ AnimalsForm/Zoo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsForm
+{
+    class Zoo
+    {
+        private List<Animal> animals = new List<Animal>();
+    }
+}
